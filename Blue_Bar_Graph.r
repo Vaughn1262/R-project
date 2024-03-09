ae239de84d6423c7c16621e8f8ca73518e5282b7
Vaughn1262 <vaughnpalmer62@gmail.com>
2024-03-09 12:48:40 -0700

    Update Blue_Bar_Graph.r

--- Blue_Bar_Graph.r
+++ Blue_Bar_Graph.r
@@ -4,5 +4,3 @@ BarData <- c(60, 5, 19, 3, 28)
 # Plot the bar chart, It is made blue and it is horizontal
 barplot(BarData, xlab = "X_axis", ylab = "Y_axis",
   col ="blue", main ="randomStuffBarChart")
-
-
